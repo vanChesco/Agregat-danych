8b08acd715a9859d43ad724c5f79fade320e9714
vanchesco <vanchesco@icloud.com>
2020-05-23 14:34:15 +0200

    Dodaj pliki projektów.

---
+++ Agregat danych.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+class Agregat
+{
+public:
+
+    int a;
+    float f;
+    std::string nazwa;
+    int r;
+};
+
+
+int main()
+{
+    Agregat ag = { 2, 3.44, "waz", 5 };
+
+    std::cout << ag.a << std::endl;
+
+    return 0;
+}
