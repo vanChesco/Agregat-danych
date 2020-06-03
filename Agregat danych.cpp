0476fefd641a63e9fc901cca1fb0788bab4dffee
vanchesco <vanchesco@icloud.com>
2020-06-03 16:41:24 +0200

    Agregat, struct, enum, enum class Porzadeczek

--- Agregat danych.cpp
+++ Agregat danych.cpp
@@ -12,17 +12,17 @@ public:							// Wszystkie zmienne musza byc publiczne.
 	int			r;				//
 };
 
-enum class Paliwo 
-{ 
+enum class Paliwo		// Definicja SILNEGO Enuma (eliminuje mozliwosc wpisania zdefiniowanej wartosci, jako liczby int, 
+{ 						// ale trzeba pamietac o zapisywaniu tych wartosci razem z zakresem ZAKRES::WARTOSC)
 	PB95, 
 	PB98, 
 	ON, 
 	LPG 
 };
 
-enum Pojemnosc 
-{ 
-	_1_4, 
+enum Pojemnosc			// Definicja ZWYKLEGO Enuma
+{ 						// w tym przypadku istnieje ryzyko, ze przykladowa wartosc typu enum _1_8 zostanie zinterpretowana jako int 2. 
+	_1_4, 				// w typie enum class tego zagrozenia nie ma.
 	_1_6, 
 	_1_8, 
 	_2_0, 
@@ -32,26 +32,27 @@ enum Pojemnosc
 	_3_0 
 };
 
-struct Auto
-{
-	std::string	marka	= "Audi", 
-				model	= "A6";
-	int			rocznik	= 2004;
-	Paliwo		fuel	= Paliwo::LPG;
-	Pojemnosc	silnik	= Pojemnosc::_2_4;  
-};
+struct Auto											// Struktora danych
+{													// mozna jej uzywac, jesli obiekt ma posiadac tylko parametry bez funkcji skladowych
+	std::string	marka	= "Audi", 					// Z definicji wszystkie parametry sa tu publiczne
+				model	= "A6";						// 
+	int			rocznik	= 2004;						// 
+	Paliwo		fuel	= Paliwo::LPG;				// 
+	Pojemnosc	silnik	= Pojemnosc::_2_4;			// 
+};													// 
 
 void pokaz(Auto autko)
 {
-	std::cout << "\n\t" << autko.marka << ' ' << autko.model << " rocznik: "<< autko.rocznik << ", paliwo: " << int(autko.fuel) << ", silnik: " << autko.silnik << "\n\n"; // konieczne rzutowanie przy enum class
+	std::cout	<< "\n\t" << autko.marka << ' ' << autko.model << " rocznik: "<< autko.rocznik 
+				<< ", paliwo: " << int(autko.fuel) << ", silnik: " << autko.silnik << "\n\n";			// konieczne rzutowanie przy enum class
 
 }
 
 int main()
 {
-	Agregat ag = { 2, 3.44f, "waz", 5 };			// Inicjalizacja zmiennych, bedacych skladowymi agregatu "ag"
+	Agregat pudelko = { 2, 3.44f, "waz", 5 };			// Inicjalizacja zmiennych, bedacych skladowymi agregatu "ag"
 
-	std::cout << "\n\t" << ag.nazwa << '\n';
+	std::cout << "\n\t" << pudelko.nazwa << '\n';
 
 	int		x{ 0 };									// inicjalizacja x, ktora moglaby rowniez byc zapisana po prostu x = 0
 	Paliwo	fuel{ Paliwo::LPG };					// inicjalizacja zmiennej typu enum; tez moglaby byc wykonana za pomoca operatora przypisania
